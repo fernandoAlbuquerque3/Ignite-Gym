8b2dcb21d20d80fd3b6e6780893864358fb68acb
fernando <fernandonetodeveloper@gmail.com>
2023-05-06 14:26:32 -0300

    svg

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ import {
 } from "@expo-google-fonts/roboto"
 
 import { Loading } from "@components/Loading"
-import { Signin } from "@screens/Signin"
+import { SignIn } from "@screens/Signin"
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -21,7 +21,7 @@ export default function App() {
         translucent
       />
 
-      {fontsLoaded ? <Signin /> : <Loading/>}
+      {fontsLoaded ? <SignIn /> : <Loading/>}
     </NativeBaseProvider>
   )
 }
--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,16 +1,23 @@
-import {VStack, Image} from 'native-base'
+import { VStack, Image, Text, Center } from "native-base"
 
-import BackgroundImg from '@assets/background.png'
+import LogoSvg from "@assets/logo.svg"
+import BackgroundImg from "@assets/background.png"
 
-export function Signin() {
+export function SignIn() {
   return (
-    <VStack flex={1} bg={'gray.700'}>
+    <VStack flex={1} bg={"gray.700"}>
       <Image
         source={BackgroundImg}
-        alt='pessoas treinando'
-        resizeMode='contain'
-        position={'absolute'}
+        alt="pessoas treinando"
+        resizeMode="contain"
+        position={"absolute"}
       />
+
+      <Center my={24}>
+        <LogoSvg />
+
+        <Text color={"gray.100"} fontSize={"sm"}>Treine sua mente e o seu corpo</Text>
+      </Center>
     </VStack>
   )
 }
