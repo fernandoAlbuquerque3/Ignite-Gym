58ec4168ae06e288efebae8d31d3b0aec334da39
fernando <fernandonetodeveloper@gmail.com>
2023-05-12 18:33:44 -0300

    routescontext

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ import {
 } from "@expo-google-fonts/roboto"
 
 import { Loading } from "@components/Loading"
-import { SignUp } from "@screens/SignUp"
+import { Routes } from "@routes/index"
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -21,7 +21,7 @@ export default function App() {
         translucent
       />
 
-      {fontsLoaded ? <SignUp /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   )
 }
