d86aed8642ad090aa4481699b8581965cb643344
fernando <fernandonetodeveloper@gmail.com>
2023-05-26 13:16:44 -0300

    homeheader

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -2,10 +2,10 @@ import { HStack, Heading, Text, VStack } from "native-base"
 
 export function HomeHeader() {
   return (
-    <HStack>
+    <HStack bg={"gray.600"} pt={16} pb={5} px={8} alignItems={"center"}>
       <VStack>
-        <Text color={"gray.100"}>Olá,</Text>
-        <Heading color={"gray.100"}>Fernando</Heading>
+        <Text color={"gray.100"} fontSize={"md"}>Olá,</Text>
+        <Heading color={"gray.100"} fontSize={"md"}>Fernando</Heading>
       </VStack>
     </HStack>
   )
