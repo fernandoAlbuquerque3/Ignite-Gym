ab71a42927d1373894f59035bb502210863841f1
fernando <fernandonetodeveloper@gmail.com>
2023-05-26 13:38:57 -0300

    userphotocomponent

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,11 +1,22 @@
 import { HStack, Heading, Text, VStack } from "native-base"
+import { UserPhoto } from "./UserPhoto"
 
 export function HomeHeader() {
   return (
     <HStack bg={"gray.600"} pt={16} pb={5} px={8} alignItems={"center"}>
+      <UserPhoto 
+        source={{uri: 'https://github.com/fernandoAlbuquerque3.png'}}
+        alt="Foto de perfil do usuário."
+        size={16}
+        mr={4}
+      />
       <VStack>
-        <Text color={"gray.100"} fontSize={"md"}>Olá,</Text>
-        <Heading color={"gray.100"} fontSize={"md"}>Fernando</Heading>
+        <Text color={"gray.100"} fontSize={"md"}>
+          Olá,
+        </Text>
+        <Heading color={"gray.100"} fontSize={"md"}>
+          Fernando
+        </Heading>
       </VStack>
     </HStack>
   )
