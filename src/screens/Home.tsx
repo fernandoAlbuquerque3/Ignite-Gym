ee00e5edbd17976f60112aad25117b78a27b94e3
fernando <fernandonetodeveloper@gmail.com>
2023-05-31 16:31:27 -0300

    groupselected

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,15 +1,27 @@
+import { useState } from "react"
 import { Group } from "@components/Group"
 import { HomeHeader } from "@components/HomeHeader"
 import { HStack, VStack } from "native-base"
 
 export function Home() {
+  
+  const [groupSelected, setGroupSelected] = useState('costa');
+
   return (
     <VStack flex={1}>
       <HomeHeader />
 
       <HStack>
-        <Group name="costas" />
-        <Group name="ombro" />
+        <Group
+          name="costas"
+          isActive={groupSelected === "costa"}
+          onPress={() => setGroupSelected("costa")}
+        />
+        <Group
+          name="ombro"
+          isActive={groupSelected === "ombro"}
+          onPress={() => setGroupSelected("ombro")}
+        />
       </HStack>
     </VStack>
   )
