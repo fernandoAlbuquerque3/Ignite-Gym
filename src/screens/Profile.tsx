05b71da2d2d2eeed0f5826dea26f79f25dcef9d2
fernando <fernandonetodeveloper@gmail.com>
2023-06-09 18:26:12 -0300

    mensagenstoast

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -6,8 +6,9 @@ import {
   Skeleton,
   Text,
   Heading,
+  useToast,
 } from "native-base"
-import { Alert, TouchableOpacity } from "react-native"
+import { TouchableOpacity } from "react-native"
 import * as ImagePicker from "expo-image-picker"
 import * as FileSystem from "expo-file-system"
 
@@ -23,6 +24,8 @@ export function Profile() {
   const [userPhoto, setUserPhoto] = useState(
     "https://github.com/fernandoAlbuquerque3.png"
   )
+
+  const toast = useToast()
 
   async function handleUserPhotoSelect() {
     setPhotoIsLoading(true)
@@ -44,7 +47,11 @@ export function Profile() {
         )
 
         if (photoInfo.exists && photoInfo.size / 1024 / 1024 > 5) {
-          return Alert.alert("Foto muito grande, escolha uma até 5 megas")
+          return toast.show({
+            title: "Essa imagem é muito grande. escolha uma de até 5MB",
+            placement: "top",
+            bgColor: "red.500",
+          })
         }
 
         setUserPhoto(photoSelected.assets[0].uri)
