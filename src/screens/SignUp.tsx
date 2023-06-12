130f26d1c893afe39ff91867a4e67f8ebb581802
fernando <fernandonetodeveloper@gmail.com>
2023-06-12 15:53:53 -0300

    reacthookforminstall

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react"
 import { useNavigation } from "@react-navigation/native"
 import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base"
 
@@ -8,10 +9,24 @@ import { Input } from "@components/Input"
 import { Button } from "@components/Button"
 
 export function SignUp() {
-  const navigation = useNavigation();
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [passwordConfirm, setPasswordConfirm] = useState("")
+
+  const navigation = useNavigation()
 
   function handleGoBack() {
     navigation.goBack()
+  }
+
+  function handleSignUp() {
+    console.log({
+      name,
+      email,
+      password,
+      passwordConfirm,
+    })
   }
 
   return (
@@ -45,15 +60,26 @@ export function SignUp() {
             Crie sua conta
           </Heading>
 
-          <Input placeholder="Nome" />
+          <Input placeholder="Nome" onChangeText={setName} />
           <Input
             placeholder="E-mail"
             keyboardType="email-address"
             autoCapitalize="none"
+            onChangeText={setEmail}
           />
-          <Input placeholder="Senha" secureTextEntry />
 
-          <Button title="Criar e acessar" />
+          <Input
+            placeholder="Senha"
+            secureTextEntry
+            onChangeText={setPassword}
+          />
+          <Input
+            placeholder="Confirme a Senha"
+            secureTextEntry
+            onChangeText={setPasswordConfirm}
+          />
+
+          <Button title="Criar e acessar" onPress={handleSignUp} />
         </Center>
 
         <Button
