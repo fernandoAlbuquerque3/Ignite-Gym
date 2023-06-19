8b6d6df42c17aaa44a49e15965743953d586bef7
fernando <fernandonetodeveloper@gmail.com>
2023-06-19 17:03:38 -0300

    estilizando_error_message

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -77,11 +77,14 @@ export function SignUp() {
               required: "Informe o nome.",
             }}
             render={({ field: { onChange, value } }) => (
-              <Input placeholder="Nome" onChangeText={onChange} value={value} />
+              <Input
+                placeholder="Nome"
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.name?.message}
+              />
             )}
           />
-
-          <Text color={"white"}>{errors.name?.message}</Text>
 
           <Controller
             control={control}
@@ -90,8 +93,8 @@ export function SignUp() {
               required: "Informe o e-mail",
               pattern: {
                 value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z] {2,}$/i,
-                message: 'E-mail inválido'
-              }
+                message: "E-mail inválido",
+              },
             }}
             render={({ field: { onChange, value } }) => (
               <Input
@@ -100,11 +103,10 @@ export function SignUp() {
                 autoCapitalize="none"
                 onChangeText={onChange}
                 value={value}
+                errorMessage={errors.email?.message}
               />
             )}
           />
-
-          <Text color={"white"}>{errors.email?.message}</Text>
 
           <Controller
             control={control}
